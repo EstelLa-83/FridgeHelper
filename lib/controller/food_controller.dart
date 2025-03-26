0a001ab453394c30f9190a8ee447145691ca09c0
EstelLa-83 <juju0448@naver.com>
2025-03-27 02:11:53 +0900

    edit 'main.dart'

---
+++ lib/controller/food_controller.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class FoodNotifier extends ChangeNotifier {
+
+}
---
+++ lib/page/page_main.dart
@@ -0,0 +1,20 @@
+import 'package:fridge/page/page_fridge.dart';
+import 'package:fridge/page/page_setting.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
