00b00eb242f3cd5e27e64c922f6910e4c7c5787f
EstelLa-83 <juju0448@naver.com>
2025-03-27 15:17:46 +0900

    define default class

--- lib/page/page_main.dart
+++ lib/page/page_main.dart
@@ -9,8 +9,24 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class Page {
+  const Page(this.iconData, this.text);
+  final IconData iconData;
+  final String text;
+}
 
+class _MainPageState extends State<MainPage> {
+  int idx = 0;
+
+  final pageList = const <Page>[
+    Page(Icons.home_filled, "fridgy"),
+    Page(Icons.person, "setting"),
+  ];
+
+  final _buildBody = <Widget>[
+    FridgePage(),
+    SettingPage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/page/page_setting.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+
+  @override
+  State<SettingPage> createState() => _FridgePageState();
+}
+
+class _FridgePageState extends State<SettingPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
