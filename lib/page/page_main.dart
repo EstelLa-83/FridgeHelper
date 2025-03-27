ce4af0078143480cedd00a25d6e558443cc451a8
EstelLa-83 <juju0448@naver.com>
2025-03-27 15:27:57 +0900

    edit main page

--- lib/page/page_main.dart
+++ lib/page/page_main.dart
@@ -19,18 +19,39 @@ class _MainPageState extends State<MainPage> {
   int idx = 0;
 
   final pageList = const <Page>[
-    Page(Icons.home_filled, "fridgy"),
+    Page(Icons.home_filled, "fridge"),
     Page(Icons.person, "setting"),
   ];
 
-  final _buildBody = <Widget>[
-    FridgePage(),
-    SettingPage(),
-  ];
+  final _buildBody = <Widget>[FridgePage(), SettingPage()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: idx,
+        onTap: (x) {
+          setState(() {
+            idx = x;
+          });
+        },
+        elevation: 20.0,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: const Color(0xff395BA9),
+        items:
+            pageList
+                .map(
+                  (Page page) => BottomNavigationBarItem(
+                    backgroundColor: Colors.white,
+                    icon: Icon(page.iconData),
+                    label: page.text,
+                  ),
+                )
+                .toList(),
+      ),
+      body: _buildBody[idx],
     );
   }
 }
