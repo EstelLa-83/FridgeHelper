8381d2e7d0b3563cec79257e685c5d1e845399cb
EstelLa-83 <juju0448@naver.com>
2025-03-28 17:48:02 +0900

    put 'food' to 'fridge_p'

--- lib/page/page_fridge.dart
+++ lib/page/page_fridge.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:fridge/widget/fridge/food_card.dart';
 
 class FridgePage extends StatefulWidget {
   const FridgePage({super.key});
@@ -8,10 +11,255 @@ class FridgePage extends StatefulWidget {
 }
 
 class _FridgePageState extends State<FridgePage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final _unfocusNode = FocusNode();
+
+  List info = [];
+
+  void saveFoodInfo(List foodList) async {
+    final prefs = await SharedPreferences.getInstance();
+    String encoded = jsonEncode(foodList); // List<Map> → JSON 문자열
+    await prefs.setString('cached_food_list', encoded);
+  }
+
+  Future<List> loadFoodInfo() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? jsonString = prefs.getString('cached_food_list');
+
+    if (jsonString != null) {
+      return jsonDecode(jsonString); // JSON 문자열 → List
+    } else {
+      return []; // 없으면 빈 리스트 반환
+    }
+  }
+
+  void addFood(Map<String, dynamic> food) {
+    setState(() {
+      info.add(food);
+    });
+    saveFoodInfo(info);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadFoodInfo().then((loaded) {
+      setState(() {
+        info = loaded;
+      });
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    _unfocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text("Fridge"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              showAddFoodDialog(context);
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 5),
+            const Divider(color: Colors.black26, height: 2),
+            const SizedBox(height: 12),
+            Expanded(
+              child: ListView.separated(
+                itemCount: info.length,
+                itemBuilder: (BuildContext context, int idx) {
+                  print(info[idx]["name"]);
+                  return Column(
+                    children: <Widget>[
+                      FridgeFoodCard(
+                        name: info[idx]["name"],
+                        expiryDate: info[idx]["expiryDate"],
+                        onDelete: () {
+                          setState(() {
+                            info.removeAt(idx);
+                          });
+                          saveFoodInfo(info);
+                        },
+                        onEdit: () {
+                          showEditDialog(context, idx);
+                        },
+                      ),
+                    ],
+                  );
+                },
+                separatorBuilder:
+                    (BuildContext context, int index) => const ColoredBox(
+                      color: Color(0xffFDFCFF),
+                      child: SizedBox(height: 5),
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showAddFoodDialog(BuildContext context) {
+    final nameController = TextEditingController();
+    DateTime? selectedDate;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: const Text('Add Food'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(labelText: 'Food Name'),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      const Text("expiryDate: "),
+                      Text(
+                        selectedDate != null
+                            ? "${selectedDate!.toLocal()}".split(' ')[0]
+                            : "Not Selected",
+                        style: TextStyle(
+                          color:
+                              selectedDate != null ? Colors.black : Colors.grey,
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.calendar_today),
+                        onPressed: () async {
+                          DateTime? picked = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(2020),
+                            lastDate: DateTime(2100),
+                          );
+                          if (picked != null) {
+                            setState(() {
+                              selectedDate = picked;
+                            });
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  child: const Text('Cancel'),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                ElevatedButton(
+                  child: const Text('Add'),
+                  onPressed: () {
+                    if (nameController.text.isNotEmpty &&
+                        selectedDate != null) {
+                      addFood({
+                        "name": nameController.text,
+                        "expiryDate": selectedDate!.toIso8601String(),
+                      });
+                      Navigator.pop(context);
+                    }
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void showEditDialog(BuildContext context, int idx) {
+    DateTime? selectedDate = DateTime.tryParse(info[idx]["expiryDate"]);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('"${info[idx]["name"]}"'),
+              content: Row(
+                children: [
+                        const Text("New expiry: "),
+                        Text(
+                          selectedDate != null
+                              ? "${selectedDate!.toLocal()}".split(' ')[0]
+                              : "Invalid",
+                          style: TextStyle(
+                            color:
+                                selectedDate != null
+                                    ? Colors.black
+                                    : Colors.grey,
+                          ),
+                        ),
+                                          IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: () async {
+                      DateTime? picked = await showDatePicker(
+                        context: context,
+                        initialDate: selectedDate ?? DateTime.now(),
+                        firstDate: DateTime(2020),
+                        lastDate: DateTime(2100),
+                      );
+                      if (picked != null) {
+                        setState(() {
+                          selectedDate = picked;
+                        });
+                      }
+                    },
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  child: const Text('Cancel'),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                ElevatedButton(
+                  child: const Text('Save'),
+                  onPressed: () {
+                    if (selectedDate != null) {
+                      setState(() {
+                        info[idx]["expiryDate"] =
+                            selectedDate!.toIso8601String();
+                      });
+                      saveFoodInfo(info);
+                      Navigator.pop(context);
+                    }
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
     );
   }
 }
--- lib/widget/fridge/food_card.dart
+++ lib/widget/fridge/food_card.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
 class FridgeFoodCard extends StatelessWidget {
-  const FridgeFoodCard(
-      {super.key, required this.name, required this.expiryDate});
+  const FridgeFoodCard({
+    super.key,
+    required this.name,
+    required this.expiryDate,
+    required this.onDelete,
+    required this.onEdit,
+  });
   final String name;
   final String expiryDate;
+  final VoidCallback onDelete;
+  final VoidCallback onEdit;
+
+  String getDdayLabel(String expiryDateStr) {
+    try {
+      DateTime expiryDate = DateTime.parse(expiryDateStr);
+      DateTime today = DateTime.now();
+      // 날짜 차이 계산 (시간 차이 무시, 날짜만 비교)
+      Duration diff = expiryDate.difference(
+        DateTime(today.year, today.month, today.day),
+      );
+
+      int dDay = diff.inDays;
+
+      if (dDay > 0) {
+        return "D-$dDay";
+      } else if (dDay == 0) {
+        return "D-Day";
+      } else {
+        return "D+${-dDay}";
+      }
+    } catch (e) {
+      return "Dday Error";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,60 +42,68 @@ class FridgeFoodCard extends StatelessWidget {
       alignment: Alignment.center,
       height: 70,
       color: const Color(0xffFDFCFF),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        Flexible( // 아이콘
-          flex: 3,
-          child: Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // 아이콘
+          Flexible(
+            flex: 3,
+            child: Container(
               margin: const EdgeInsets.all(5.0),
-              child: const Icon(Icons.food_bank_outlined)),
-        ),
-        Flexible( // 텍스트
-          flex: 10,
-          child: Container(
-            width: 210,
-            margin: const EdgeInsets.only(left: 10, right: 10, top: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name, // 식품명
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
-                ),
-                SizedBox(
-                  height: 2,
-                ),
-                Text(
-                  expiryDate, // 유통기한
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    fontSize: 15,
+              child: const Icon(Icons.food_bank_outlined),
+            ),
+          ),
+          // 텍스트
+          Flexible(
+            flex: 10,
+            child: Container(
+              width: 210,
+              margin: const EdgeInsets.only(left: 10, right: 10, top: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 식품명
+                  Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
                   ),
-                )
-              ],
+                  SizedBox(height: 2),
+                  // 유통기한
+                  Text(
+                    // 디데이로 표시
+                    getDdayLabel(expiryDate),
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        Flexible( // 삭제 버튼
-          flex: 2,
-          child: Container(
-            child: IconButton(
-              icon: const Icon(Icons.delete_outline_rounded),
-              onPressed: () {},
+          // 삭제 버튼
+          Flexible(
+            flex: 2,
+            child: Container(
+              child: IconButton(
+                icon: const Icon(Icons.delete_outline_rounded),
+                onPressed: onDelete,
+              ),
             ),
           ),
-        ),
-        Flexible( // 수정 버튼
+          // 수정 버튼
+          Flexible(
             flex: 2,
             child: Container(
               child: IconButton(
                 icon: const Icon(Icons.create_outlined),
-                onPressed: () {},
+                onPressed: onEdit,
               ),
-            ))
-      ]),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
