6bfb52e0bd581530fa4aa254027077238d915970
pushow <qkrehdals55@gmail.com>
2025-04-14 11:33:50 +0900

    잠금화면에서 알림을 누르면 앱이 바로 뜨는 현상 수정

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   tz.initializeTimeZones();
 
-  await requestExactAlarmPermissionIfNeeded();
   await requestNotificationPermissionIfNeeded();
 
   const AndroidInitializationSettings initializationSettingsAndroid =
