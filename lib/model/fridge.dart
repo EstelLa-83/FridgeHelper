3c3b0867cf447748e4b37bbd76d9bf5539dfdd27
EstelLa-83 <juju0448@naver.com>
2025-04-18 01:46:27 +0900

    delete member field called fridgeType

--- lib/model/fridge.dart
+++ lib/model/fridge.dart
@@ -1,10 +1,8 @@
 class Fridge {
   int fridgeId;
   String fridgeName;
-  String fridgeType;  // All / Cold / Frozen
 
   Fridge(
       {required this.fridgeId,
-      required this.fridgeName,
-      required this.fridgeType});
+      required this.fridgeName,});
 }
