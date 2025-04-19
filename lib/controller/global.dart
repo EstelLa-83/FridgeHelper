2fe4cfd2b8a67031b1806f617fe135b694f62420
EstelLa-83 <juju0448@naver.com>
2025-04-19 23:40:46 +0900

    Add profile images to assets

--- lib/controller/global.dart
+++ lib/controller/global.dart
@@ -3,3 +3,4 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 final FlutterSecureStorage storage = FlutterSecureStorage();
 
 const String BASE_URL = "http://192.168.0.11:8080";
+const String BASE_IMAGE = "assets/images/profiles";
