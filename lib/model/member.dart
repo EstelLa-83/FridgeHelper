452c0f5c6f544f17f4d6e10ed44435caf936fd87
EstelLa-83 <juju0448@naver.com>
2025-04-20 01:25:13 +0900

    add member's profile to model of member

--- lib/model/member.dart
+++ lib/model/member.dart
@@ -1,9 +1,11 @@
 class Member {
   int memberId;
   String memberName;
+  String memberProfile;
 
   Member({
     required this.memberId,
     required this.memberName,
+    required this.memberProfile,
   });
 }
