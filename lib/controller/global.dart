272c35b4d478cd85b6a1d5dc57c03e63a3d7fb10
pushow <55828632+pushow@users.noreply.github.com>
2025-04-21 05:32:35 +0900

    remove address

--- lib/controller/global.dart
+++ lib/controller/global.dart
@@ -2,6 +2,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 final FlutterSecureStorage storage = FlutterSecureStorage();
 
-const String BASE_URL = "http://192.168.0.11:8080";
+const String BASE_URL = ""; // should edit this
 
 bool onServer = true;
